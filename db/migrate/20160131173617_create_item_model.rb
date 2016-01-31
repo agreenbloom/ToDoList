b8dc5c48f00c17873c938a26f75b0c9980f2fb01
Abby <abby@Bitmaker-6.local>
2016-01-31 12:42:34 -0500

    create schema for items

---
+++ db/migrate/20160131173617_create_item_model.rb
@@ -0,0 +1,10 @@
+class CreateItemModel < ActiveRecord::Migration
+  def change
+    create_table :items do |t|
+  		t.string   "title"
+  		t.text "description"  	
+      t.timestamps null: false
+
+    end
+  end
+end
