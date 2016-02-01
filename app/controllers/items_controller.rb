c93e2177a5e662bc4ae15c057fdaba3b7f336db2
Abby <abby@Bitmaker-6.local>
2016-02-01 13:54:14 -0500

    basic decorator working - labels if item is pending, completed or won't be complete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,8 @@ class ItemsController < ApplicationController
 
   def index
     @q = Item.ransack(params[:q])
-    @items = @q.result(distinct: true)
+    @items = @q.result(distinct: true).decorate
+
   end
 
   def show
