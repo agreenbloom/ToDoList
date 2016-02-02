5e28986c989aeb0b7877c3a96527a9f27835641e
Abby <abby@Bitmaker-6.local>
2016-02-02 10:59:23 -0500

    basic activeadmin account working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,17 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, 
+         :recoverable, :rememberable, :trackable, :validatable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, 
   :recoverable, :rememberable, :trackable, :validatable
 
   has_many :items
 
-  def owner?
-  	self.role.name == "Owner"
+  def role?(r)
+  	role.inclue? r.to_s
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  # devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  devise_for :users, ActiveAdmin::Devise.config
 	root "items#index"
   resources :items, except: [:edit, :update]
 
