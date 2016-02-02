5cd4dcfe8c3ba41b05fc48c585aa094497902f0d
Abby <abby@Bitmaker-6.local>
2016-02-02 11:21:58 -0500

    edit user ability if admin with cancancan

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,11 +3,15 @@ class Ability
 
   def initialize(user)
     # Define abilities for the passed in user here. For example:
-    #
+    # #
+    user ||=User.new
     if user.id 
       # can :manage, Item, id: user.id
-      can [:index, :show, :create, :new], Item, user_id: user.id
+      can [:index, :show], Item, user_id: user.id
+    elsif user.role == 'admin'
+    	can :manage, :all
     end
+
   end
     
 
