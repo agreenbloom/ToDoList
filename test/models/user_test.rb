3e987a04cce2fcd7c44fb53a62e0588f033b2e27
Abby Greenbloom <abby.greenbloom@gmail.com>
2016-02-01 20:39:13 -0500

    initial user tests passing

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,24 @@
-require 'test_helper'
-
+require "test_helper"
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  describe User do
+
+    let(:user_params) { { email: "john@example.com", password: "password" } }
+    let(:user) { User.new user_params }
+
+    it "is valid with valid params" do
+
+      user.must_be :valid? # Must create with valid params
+    end
+
+    it "is invalid without an email" do
+      # Delete email before user let is called
+      invalid_params = {}
+      u = User.new(invalid_params)
+
+      u.wont_be :valid? # Must not be valid without email
+      u.errors[:email].must_be :present? # Must have error for missing email
+    end
+
+  end
 end
