fed7b5f35eb9106349887c050f41bdc292405348
Abby <abby@Bitmaker-6.local>
2016-02-02 09:56:13 -0500

    temporarily commenting out cancancan code until two branches are merged

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  include CanCan::ControllerAdditions
+  # include CanCan::ControllerAdditions
 
   # rescue_from CanCan::AccessDenied do |exception|
   #   flash[:warning] = exception.message
