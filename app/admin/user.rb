7ee935b75ff9a0814d6f086fe79caf672ad7daab
Abby <abby@Bitmaker-6.local>
2016-02-02 11:09:24 -0500

    add items to active admin dashboard

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -19,8 +19,6 @@ ActiveAdmin.register User do
   form do |f|
     f.inputs "Admin Details" do
       f.input :email
-      f.input :password
-      f.input :password_confirmation
     end
     f.actions
   end
