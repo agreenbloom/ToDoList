149686c21a86f1b9ed2c6be1248312752d84b89c
Abby <abby.greenbloom@gmail.com>
2016-02-04 13:58:11 -0500

    re-added user_id to items and logout works from user page but not admin dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: {sessions: 'users/sessions', registrations: 'users/registrations', passwords: 'users/passwords'}, path_names: { sign_in: 'login', sign_out: 'logout' }
   devise_scope :user do
-    get "login", to: "devise/sessions#new"
+    get 'logout', :to => 'devise/sessions#destroy', method: :delete
+    get "login", :to => "devise/sessions#new"
     authenticated :user do
       root :to => 'admin/dashboard#index', as: :authenticated_root
     end
